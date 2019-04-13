3c23c23941003771c5c6fee02f71abb416bfc369
TahaOzll <49563007+TahaOzll@users.noreply.github.com>
2019-04-13 17:26:53 +0300

    Update oduncu.js

--- komutlar/oduncu.js
+++ komutlar/oduncu.js
@@ -4,17 +4,17 @@ const Discord = require('discord.js');
 exports.run = function(client, message) {
  
     const embed = new Discord.RichEmbed()
-        .setDescription("**WOODIE**")
+        .setDescription("**Oduncu**")
         .setImage("https://vignette.wikia.nocookie.net/dont-starve-game/images/2/23/Woodie.png/revision/latest/scale-to-width-down/350?cb=20140330212814")
         .setThumbnail("https://vignette.wikia.nocookie.net/dont-starve-game/images/2/23/Woodie.png/revision/latest/scale-to-width-down/350?cb=20140330212814")
-        .setColor(0x00AE86)
+        .setColor(0xD97634)
         .addField("Lakabı", "The Lumberjack (Oduncu)", true)
         .addField("Yetenekleri", `
         *Çok güzel bir baltası var
-   *Korkunç bir sırrı var (Werebeaver)
+   *Korkunç Bir Yaratık
    `, true)
-   .addField("Motto", `That's nice tree, eh? (güzel ağaç, ha?)`, true)
-   .addField("selam", "haha naber")
+   .addField("Kaç", `Adam Ağçaları Kesiyor Hemde Sebebsiz Yere`, true)
+   .addField("Oduncudan Kaçın", "Ondan Kaçmazsanız Sizi Yiyebilir")
    
 
    message.channel.send(embed)
